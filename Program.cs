879f8ee280c57a71e9cc39ba0082f5dd58296ef1
biancaserafim <biancaserafim368@gmail.com>
2025-10-28 17:51:46 -0300

    Adicionando código-fonte CadastroBandas

---
+++ Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+namespace CadastroBandas
+{
+    class Program
+    {
+        const string NOME_ARQUIVO = "bandas.txt";
+
+        static void addBanda(List<Banda> listaBandas)
+        {
+            Banda novaBanda = new Banda();
+            Console.Write("Nome:");
+            novaBanda.nome = Console.ReadLine();
+            Console.Write("Genero:");
+            novaBanda.genero = Console.ReadLine();
+            Console.Write("Integrantes:");
+            novaBanda.integrantes = int.Parse(Console.ReadLine());
+            Console.Write("Ranking:");
+            novaBanda.ranking = int.Parse(Console.ReadLine());
+            listaBandas.Add(novaBanda);
+            Console.WriteLine("--------");
+        }
+
+        static void mostrarBandas(List<Banda> listaBandas)
+        {
+            int posicao = 1;
+            foreach (Banda b in listaBandas)
+            {
+                Console.WriteLine($"*** Banda {posicao}***");
+                Console.WriteLine($"{b.nome} - {b.genero} - {b.integrantes} - {b.ranking}");
+                posicao++;
+            }
+        }
+        static int menu()
+        {
+            int opcao;
+            Console.WriteLine("*** Sistema de Cadastro de Bandas 4U***");
+            Console.WriteLine("1- Adicionar Banda");
+            Console.WriteLine("2- Mostrar Bandas");
+            Console.WriteLine("0- Sair do Sistema");
+            opcao = int.Parse(Console.ReadLine());
+            return opcao;
+        }
+
+        static void salvarDados(List<Banda> listaBandas, string nomeArquivo)
+        {
+            using (System.IO.StreamWriter writer = new System.IO.StreamWriter(nomeArquivo))
+            {
+                foreach (Banda b in listaBandas)
+                {
+                    writer.WriteLine($"{b.nome},{b.genero},{b.integrantes},{b.ranking}");
+                }
+            }
+            Console.WriteLine("Dados salvos com sucesso!");
+        }
+
+        static void carregarDados(List<Banda> listaBandas, string nomeArquivo)
+        {
+            if (System.IO.File.Exists(nomeArquivo))
+            {
+                string[] linhas = System.IO.File.ReadAllLines(nomeArquivo);
+                foreach (string linha in linhas)
+                {
+                    string[] campos = linha.Split(',');
+                    Banda novaBanda = new Banda();
+                    novaBanda.nome = campos[0];
+                    novaBanda.genero = campos[1];
+                    novaBanda.integrantes = int.Parse(campos[2]);
+                    novaBanda.ranking = int.Parse(campos[3]);
+                    listaBandas.Add(novaBanda);
+                }
+                Console.WriteLine("Dados carregados com sucesso!");
+            }
+            else
+                Console.WriteLine("Arquivo não encontrado :(");
+        }
+
+        static void Main()
+        {
+            List<Banda> listaBandas = new List<Banda>();
+            
+            carregarDados(listaBandas, NOME_ARQUIVO);
+            Console.WriteLine("Pressione qualquer tecla para continuar...");
+            Console.ReadKey();
+            Console.Clear();
+            
+            int opcao = 0;
+            do
+            {
+                opcao = menu();
+                switch (opcao)
+                {
+                    case 1:
+                        addBanda(listaBandas);
+                        break;
+                    case 2:
+                        mostrarBandas(listaBandas);
+                        break;
+                    case 0:
+                        salvarDados(listaBandas, NOME_ARQUIVO);
+                        Console.WriteLine("Até mais ;)");
+                        break;
+                }
+                Console.ReadKey();
+                Console.Clear(); 
+            } while (opcao != 0);
+        }
+    }
+}
